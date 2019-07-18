f719c86f9c093822c492945b06882419b826dace
Ade Yahya Prasetyo <adeyahyaprasetyo@gmail.com>
2019-07-18 22:51:25 +0700

    install apollo client

--- www/src/App.js
+++ www/src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import ApolloClient from "apollo-boost";
+import queryString from 'query-string';
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
+import { ApolloProvider } from "react-apollo";
 import Container from '@material-ui/core/Container';
 import AppBar from './components/AppBar';
 import routes from './routes';
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
 function App() {
   return (
-    <div>
+    <React.Fragment>
       <Router>
-        <div>
-          <AppBar />
-          <Container>
-            {routes.map((route, idx) => (
-              <Route {...route} />
-            ))}
-          </Container>
-        </div>
+        <ApolloProvider client={client}>
+          <ApolloHooksProvider client={client}>
+            <div>
+              <AppBar />
+              <Container>
+                {routes.map((route, idx) => (
+                  <Route {...route} />
+                ))}
+              </Container>
+            </div>
+          </ApolloHooksProvider>
+        </ApolloProvider>
       </Router>
-    </div>
+    </React.Fragment>
   );
 }
 
