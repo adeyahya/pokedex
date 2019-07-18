45a4624afca21de04d52789c1440e2b08bc4b30b
Ade Yahya Prasetyo <adeyahyaprasetyo@gmail.com>
2019-07-18 22:44:38 +0700

    install material-ui

--- www/src/App.js
+++ www/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Container from '@material-ui/core/Container';
+import AppBar from './components/AppBar';
 import routes from './routes';
 
 function App() {
   return (
     <div>
       <Router>
-        {routes.map((route, idx) => (
-          <Route {...route} />
-        ))}
+        <div>
+          <AppBar />
+          <Container>
+            {routes.map((route, idx) => (
+              <Route {...route} />
+            ))}
+          </Container>
+        </div>
       </Router>
     </div>
   );
