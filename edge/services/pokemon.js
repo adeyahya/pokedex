817771c15f24d2b6e8383883ba81f1afeb7c1488
Ade Yahya Prasetyo <adeyahyaprasetyo@gmail.com>
2019-07-20 01:03:13 +0700

    add filter mechanism

--- edge/services/pokemon.js
+++ edge/services/pokemon.js
@@ -1,12 +1,27 @@
 const pokemons = require('../data/pokemons.json');
+const idx = require('idx');
 
 module.exports = {
   getPokemons: (args = {}) => {
     const page = args.page || 1;
     const pageSize = args.pageSize || 20;
+    const filterByName = idx(args, _ => _.filter.name);
     const startPosition = (page - 1) * pageSize;
     const endPosition = (page * pageSize) - 1;
-    const items = pokemons.slice(startPosition, endPosition);
+    let items = pokemons;
+
+    if (filterByName) {
+      items = items.filter(
+        item => item
+          .name
+          .toLowerCase()
+          .indexOf(
+            filterByName.toLowerCase()
+          ) !== -1
+      );
+    } else {
+      items = items.slice(startPosition, endPosition);
+    }
     const total = pokemons.length; 
     const totalPage = Math.ceil(total / pageSize);
     return Promise.resolve({
