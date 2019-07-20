85d89161394f21a692f79c3966dfb06f5a0c2430
Ade Yahya Prasetyo <adeyahyaprasetyo@gmail.com>
2019-07-20 20:32:12 +0700

    remove unused code

--- www/src/components/ObjectTable.js
+++ www/src/components/ObjectTable.js
@@ -6,7 +6,6 @@ import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
-import { makeStyles } from "@material-ui/core/styles";
 
 import objectKeyNormalizer, { isObject, isArray, isString } from '../utils/objectNormalizer';
 
